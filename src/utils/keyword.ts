aaa34e4b9b24652784962758264650a267fdd9cd
Jennifer Khang <jenniferkhang07@gmail.com>
2026-05-27 20:06:34 +0700

    fix: fallback for nonexistent map

--- src/utils/keyword.ts
+++ src/utils/keyword.ts
@@ -117,10 +117,16 @@ export const normalCharMap: { [k: string]: string[] } = {};
 Object.keys(lookalike).forEach(key => {
     lookalike[key as keyof typeof lookalike].forEach(value => {
         let mapKey = value.toLowerCase() as keyof typeof normalCharMap;
+        if (!normalCharMap[mapKey]) {
+            normalCharMap[mapKey] = [];
+        }
         normalCharMap[mapKey].push(key);
     });
     otherLookalike[key as keyof typeof otherLookalike].forEach(value => {
         let mapKey = value.toLowerCase() as keyof typeof normalCharMap;
+        if (!normalCharMap[mapKey]) {
+            normalCharMap[mapKey] = [];
+        }
         normalCharMap[mapKey].push(key);
     });
 });
