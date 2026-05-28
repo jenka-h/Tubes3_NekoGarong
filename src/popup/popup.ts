f6218f91182bb04559c17a8990ffa487c7d2a735
Shui <faiq.azzam@gmail.com>
2026-05-28 23:41:14 +0700

    feat: popup display

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,7 +1,50 @@
 // This will be use for overall statistics.
+import "../style.css";
+import Chart from 'chart.js/auto';
+import type { ChartConfiguration } from "chart.js";
 
-const stats = document.getElementById("stats");
+const style = getComputedStyle(document.documentElement);
+// const accent = style.getPropertyValue("--accent").trim();
+const accentBg = style.getPropertyValue("--accent-bg").trim();
+const accentBorder = style.getPropertyValue("--accent-border").trim();
+const status = document.getElementById("status");
+const selectAlgorithm = document.getElementsByName("algorithm")
+const canvas = document.getElementById("keywords-chart") as HTMLCanvasElement | null;
 
-if (stats) {
-    stats.textContent = "Extension loaded successfully";
+if (status) {
+    status.textContent = "Extension ready to use ദ്ദി(• ⩊ •マ";
+}
+
+const labels = ["none", "nothing", "no", "empty"];
+const data = {
+  labels: labels,
+  datasets: [{
+    axis: 'y',
+    data: [100, 90, 230, 30],
+    fill: false,
+    backgroundColor: accentBg,
+    borderColor: accentBorder,
+    borderWidth: 1
+  }]
+};
+
+const config:ChartConfiguration<"bar", number[], string> = {
+    type: 'bar',
+    data: data,
+    options: {
+        indexAxis: 'y',
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+};
+
+if (canvas) {
+    new Chart(canvas, config);
+}
+
+if (selectAlgorithm) {
+    // idk maybe run the algorithm
 }
